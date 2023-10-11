cfdd039bb6f64d483ac5f0b8df06f233633821ec
SamuelAvella <saveper1907@g.educaand.es>
2023-10-11 08:26:39 +0200

    ejercicios

---
+++ Ejercicio12.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int punto = 0;
+        System.out.println("Cuestionario básico");
+        System.out.println("1. De que esta compueso el sistema informatico:");
+        System.out.println("a)Software b)Hardware c)Ambos");
+        System.out.print("Respuesta: ");
+        String uno = sc.nextLine();
+        uno.toLowerCase();
+        if (uno=="c") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String dos = sc.nextLine();
+        dos.toLowerCase();
+        if (dos=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print(" Respuesta: ");
+        String tres = sc.nextLine();
+        tres.toLowerCase();
+        if (tres=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String cuatro = sc.nextLine();
+        cuatro.toLowerCase();
+        if (cuatro=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String cinco = sc.nextLine();
+        cinco.toLowerCase();
+        if (cinco=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String seis = sc.nextLine();
+        seis.toLowerCase();
+        if (seis=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String siete = sc.nextLine();
+        siete.toLowerCase();
+        if (siete=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String ocho = sc.nextLine();
+        ocho.toLowerCase();
+        if (dos=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String nueve = sc.nextLine();
+        nueve.toLowerCase();
+        if (nueve=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+        System.out.println("2. ¿Como definimos un numero real con dos decimales?");
+        System.out.println("a)int b)double c)float");
+        System.out.print("Respuesta: ");
+        String diez = sc.nextLine();
+        diez.toLowerCase();
+        if (diez=="b") {
+            System.out.println("Correcta");
+            punto++;
+        } else {
+            System.out.println("Incorrecto");
+        }
+
+        System.out.print("Tu puntuación es igual a: "+punto);
+        sc.close();
+    }
+}
