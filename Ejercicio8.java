3dd450e38e78d4b4ca4d6b24470eff1de796ae21
SamuelAvella <saveper1907@g.educaand.es>
2023-10-11 09:44:46 +0200

    cambios ej8

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -8,23 +8,21 @@ public class Ejercicio8 {
         double segunda = sc.nextDouble();
         double tercera = sc.nextDouble();
         double media = (primera+segunda+tercera)/3;
-        if (media>9) {
-            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, sobresaliente.\n",primera,segunda,tercera,media);
-        } else {
-            if (media<=7&media>=8) {
-                System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, notable.\n",primera,segunda,tercera,media);
-            } else {
-                if (media>=6&media<7) {
-                    System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, bien.\n",primera,segunda,tercera,media);
-                } else {
-                    if (media>=5&media<6) {
-                        System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, suficiente.\n",primera,segunda,tercera,media);
-                    } else {
-                        System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, insuficiente.\n",primera,segunda,tercera,media);
-                    }
-                }
-            }
+        if (media>=9) {
+            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, un sobresaliente.\n",primera,segunda,tercera,media);
         }
+        if (media<=7&media>=8) {
+            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, un notable.\n",primera,segunda,tercera,media);
+        }     
+        if (media>=6&media<7) {
+            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, un bien.\n",primera,segunda,tercera,media);
+        }        
+        if (media>=5&media<6) {
+            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, un suficiente.\n",primera,segunda,tercera,media);
+        }
+        if (media<5){
+            System.out.printf("La media de %.2f %.2f y %.2f es %.2f, es decir, un insuficiente.\n",primera,segunda,tercera,media);
+        }                
         sc.close();
     }
 }
